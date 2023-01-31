1ed493a5ff2820e054b95e090c8cd6933a397fc8
sangharsh19 <sangharshgurjar49@gmail.com>
2023-01-31 18:01:45 +0530

    git commit 'first'

---
+++ app/src/main/java/com/example/samplenavigationapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.samplenavigationapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout=findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.NavigationView);
+        toolbar=findViewById(R.id.toolbar);
+
+        // setup toolbar
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new
+                ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.OpenDrawer,R.string.CloseDrawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id =item.getItemId();
+                if(id==R.id.DummyItem1){
+                    // do what you want to like call a function or fragment
+                }
+                else if(id==R.id.DummyItem2){
+
+                }
+                else {
+
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
